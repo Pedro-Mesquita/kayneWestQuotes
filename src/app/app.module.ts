5f36614483395f7a909ed818839a64bbfe8fd3f8
Pedro Mesquita <pedro.mesquitamaia@gmail.com>
2024-01-04 00:22:23 -0300

    file structure complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './features/menu/menu.component';
+import { AboutComponent } from './features/about/about.component';
+import { HomeComponent } from './features/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
